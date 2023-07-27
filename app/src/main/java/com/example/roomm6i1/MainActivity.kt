d0c8a620fe75ba883c1a238cd1d783f8f5281874
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-26 20:21:51 -0400

    se agrega los Nav y fragment

--- app/src/main/java/com/example/roomm6i1/MainActivity.kt
+++ app/src/main/java/com/example/roomm6i1/MainActivity.kt
@@ -3,6 +3,19 @@ package com.example.roomm6i1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+[ ] Navigation
+ [ ] Fragmento agregar
+ [ ] Fragmento listado
+ [ ] Crear Interfaz
+ [ ] Room
+      [ ] _Dao
+      [ ] _Base Datos
+      [ ]_Entity
+ [ ] Fragmento listado
+ [ ] RecyclerView
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
