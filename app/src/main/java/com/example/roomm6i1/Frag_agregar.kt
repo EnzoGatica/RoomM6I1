cc6ad8c6bad2e9eee72d3706d0ed3c0ace43ce74
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-28 19:32:06 -0400

    VM listo

--- app/src/main/java/com/example/roomm6i1/Frag_agregar.kt
+++ app/src/main/java/com/example/roomm6i1/Frag_agregar.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.roomm6i1.databinding.FragmentAgregarBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,28 +15,19 @@ import kotlinx.coroutines.launch
 class Frag_agregar : Fragment() {
 
     lateinit var binding: FragmentAgregarBinding
-    lateinit var repositorio: Repositorio
-
+    private val tareaVM: TareaVM by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAgregarBinding.inflate(layoutInflater,container,false)
-        initRepositorio()
         initListener()
         cargarTareas()
         return binding.root
     }
-
-    private fun initRepositorio() {
-        repositorio = Repositorio(TareaBaseDatos.getDatabase(requireContext()).getTaskDao())
-    }
-
     private fun initListener() {
         binding.btnAgregar.setOnClickListener {
             val texto = binding.editText.text.toString()
@@ -44,22 +36,15 @@ class Frag_agregar : Fragment() {
             Toast.makeText(requireContext(), "Se a agregado un texto", Toast.LENGTH_SHORT).show()
         }
     }
-
     private fun guardarTexto(text: String) {
-
         val tarea = Tarea(text, " ")
-        GlobalScope.launch { repositorio.insertTask(tarea) }
-
+        tareaVM.insertarTarea(tarea)
     }
-
     private fun cargarTareas(){
-
-        repositorio.getTareas().observe(requireActivity()){
+        tareaVM.obtenerTareas().observe(viewLifecycleOwner){
             val tareaAText = it.joinToString("\n") { it.nombre } // convierte el formato
             binding.textView.text = tareaAText // asigna los datos
         }  // recuperar la tarea
-
-
     }
 
 
